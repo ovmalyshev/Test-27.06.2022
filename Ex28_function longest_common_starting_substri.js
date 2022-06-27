ba84906724fc15a1b3abdbf38a499300adc41e02
Oleg <all-all@ukr.net>
2022-06-27 21:09:57 +0300

    3rd in 2br

--- Ex28_function longest_common_starting_substri.js
+++ Ex28_function longest_common_starting_substri.js
@@ -1,12 +1,12 @@
 function longest_common_starting_substring(arr1){
     var arr= arr1.concat().sort(),
-    a1= arr[0], a2= arr[arr.length-1], L= a1.length, i= 0;
+    
     while(i< L && a1.charAt(i)=== a2.charAt(i)) i++;
         return a1.substring(0, i);
     }
-    
+    // sdfsdf
     console.log(longest_common_starting_substring(['google', 'go'])); 
+    // sdfdsf
     
-    console.log(longest_common_starting_substring(['SQLInjection', 'SQLTutorial'])); 
     
     console.log(longest_common_starting_substring(['abcd', '1234'])); 
