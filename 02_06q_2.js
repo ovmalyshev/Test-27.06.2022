7a57c5a4c7ab314f4151644fbde55243321e3871
Oleg <all-all@ukr.net>
2022-06-27 20:42:10 +0300

    second in master

--- 02_06q_2.js
+++ 02_06q_2.js
@@ -1,18 +1,15 @@
 function weIrDStRiNgCaSe(str) {
     var arr = str.split(' ');
-    var newArr
-    // if (arr[0].charAt(0).toUpperCase() !== arr[0].charAt(0)) {
-    //     arr[0] = arr[0].charAt(0).toUpperCase() + arr[0].slice(1)
-    // }
+    var newArr1
     for (var i = 0; i < arr.length; i++) {
-       
             arr[i] = arr[i].split('').map(function(value, index) {
-                return index % 2 == 0 ? value.toUpperCase() : value.toLowerCase();
+                return index % 2 === 0 ? value.toUpperCase() : value.toLowerCase();
                 }).join('') 
        
         }
-    
-    var newStr = arr.join(' ');
+// console
+        var newStr = arr.join(' ');
     console.log(newStr);
 }
 weIrDStRiNgCaSe('String');
+// xfxcvxvc
